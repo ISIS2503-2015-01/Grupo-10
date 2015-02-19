35354498189747d434f7d563804c703aae7194e6
kiloxoqp <jorgeadelosrios@gmail.com>
2015-02-18 20:46:48 -0500

    Se agregaron las pruebas con un pantallazo en la carpeta de servicios. Ultimo commit

--- pscm.servicios/pscm.servicios/src/main/java/grupo10/pscm/servicios/PacientesResource.java
+++ pscm.servicios/pscm.servicios/src/main/java/grupo10/pscm/servicios/PacientesResource.java
@@ -5,7 +5,11 @@
  */
 package grupo10.pscm.servicios;
 
+import grupo10.pscm.exceptions.OperacionFallidaException;
 import grupo10.pscm.logica.ejb.ServicioPacientes;
+import grupo10.pscm.models.Paciente;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.PathParam;
@@ -13,8 +17,11 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  * REST Web Service
@@ -54,6 +61,21 @@ public class PacientesResource {
     {
         //TODO convert to JSON
         return servPacientes.getPaciente("id").toString();
+    }
+    
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    public void createPaciente(String nombre,String apellido,String telefonoCelular,String id,String telefonoFijo,String genero,String direccion) throws OperacionFallidaException, Exception
+    {
+        try
+        {
+         Paciente p = new Paciente(nombre, apellido, telefonoFijo, direccion, telefonoFijo, genero, direccion);
+         servPacientes.agregarPaciente(p);
+        }
+        catch(OperacionFallidaException ex)
+        {
+             throw new OperacionFallidaException(ex.getMessage());
+        } 
     }
     
     /**
